96972293af85662725900ce45f4b9559389bb534
Ben-Gun <dmitriy.titenok@gmail.com>
2023-01-12 15:25:29 +0300

    dz 5.01

--- Files.py
+++ Files.py
@@ -1,15 +1,27 @@
-import os
-
 if __name__ == '__main__':
-    li1 = []
     num = 0
-    for i in open('text.txt', 'r'):
+    res = []
+    li = []
+    temp = 0
+    for i in open('text.txt', 'r+'):
         li = i.split(' ')
         num = int(li[0])
+        temp = int(li[-1])
         for j in range(2, len(li)-2, 2):
+            if li[j-1] == '*':
+                num *= int(li[j])
+            if li[j-1] == '/':
+                num //= int(li[j])
             if li[j-1] == '-':
                 num -= int(li[j])
             if li[j-1] == '+':
                 num += int(li[j])
+        if temp == num:
+            res.append('True')
+        else:
+            res.append('False')
+        f = open('output.txt', 'w+')
+        f.write(str(res))
+        f.close()
         print(num)
         num = 0
--- main.py
+++ main.py
@@ -1,15 +1,39 @@
-import os
-
 if __name__ == '__main__':
-    li1 = []
     num = 0
-    for i in open('text.txt', 'r'):
+    res = []
+    li = []
+    temp = 0
+    decision = 0
+    for i in open('text.txt', 'r+'):
         li = i.split(' ')
-        num = int(li[0])
+        decision = int(li[-1])
+        for j in range(1, len(li)-2, 2):
+            if li[j] == '*':
+                temp = int(li[j-1]) * int(li[j+1])
+                continue
+            elif li[j] == '/':
+                temp = int(li[j-1]) // int(li[j+1])
+                continue
+            if li[j] == '*':
+                temp = temp * int(li[j + 1])
+            elif li[j] == '/':
+                temp = temp // int(li[j + 1])
         for j in range(2, len(li)-2, 2):
+            if li[j-1] == '*':
+                continue
+            if li[j-1] == '/':
+                continue
             if li[j-1] == '-':
                 num -= int(li[j])
             if li[j-1] == '+':
-                num += int(li[j])
+                num = int(li[j])
+        num = num + temp
+        if decision == num:
+            res.append('True')
+        else:
+            res.append('False')
+        f = open('output.txt', 'w+')
+        f.write(str(res))
+        f.close()
         print(num)
         num = 0
