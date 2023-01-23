7be2210a620a4b0a1d12366bbc9009694447b2da
Ben-Gun <dmitriy.titenok@gmail.com>
2023-01-23 16:57:14 +0300

    23.01

--- vsiakoe.py
+++ vsiakoe.py
@@ -1,9 +1,50 @@
-def is_prime(x):
-    for i in range(2, (x//2)+1):
-        if x % i == 0:
-            return False
-    return True
+import math
 
-x = int(input())
 
-print(is_prime(x))
+def arg_min(T, S):
+    amin = -1
+    m = math.inf  # максимальное значение
+    for i, t in enumerate(T):
+        if t < m and i not in S:
+            m = t
+            amin = i
+
+    return amin
+
+
+D = ((0, 2, 0, 4),
+     (2, 0, 1, 0),
+     (0, 1, 0, 3),
+     (4, 0, 3, 0))
+
+N = len(D)  # число вершин в графе
+T = [math.inf]*N   # последняя строка таблицы
+
+v = 0       # стартовая вершина (нумерация с нуля)
+S = {v}     # просмотренные вершины
+T[v] = 0    # нулевой вес для стартовой вершины
+M = [0]*N   # оптимальные связи между вершинами
+
+while v != -1:          # цикл, пока не просмотрим все вершины
+    for j, dw in enumerate(D[v]):   # перебираем все связанные вершины с вершиной v
+        if j not in S:           # если вершина еще не просмотрена
+            w = T[v] + dw
+            if w < T[j]:
+                T[j] = w
+                M[j] = v        # связываем вершину j с вершиной v
+
+    v = arg_min(T, S)            # выбираем следующий узел с наименьшим весом
+    if v >= 0:                    # выбрана очередная вершина
+        S.add(v)                 # добавляем новую вершину в рассмотрение
+
+#print(T, M, sep="\n")
+
+# формирование оптимального маршрута:
+start = 2
+end = 4
+P = [end]
+while end != start:
+    end = M[P[-1]]
+    P.append(end)
+
+print(P)
