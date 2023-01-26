1bc9e0e9beb7eded10099615f4abc542294671b7
Ben-Gun <dmitriy.titenok@gmail.com>
2023-01-26 17:34:46 +0300

    Бот погоды телеграмм

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,38 +1,71 @@
-import json
-import telebot
 import requests
+import datetime
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 
 token = '5868468005:AAHoWglbRfawzNGufvA3mJ1xtr0-PX4j8GM'
-bot = telebot.TeleBot(token)
+open_weather_token = 'a4f5f49533e6570aa7045c937b243151'
+bot = Bot(token=token)
+dp = Dispatcher(bot)
 
-@bot.message_handler(commands=['start'])
-def helloMessage(message):
-    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button = telebot.types.KeyboardButton('Минск')
-    button1 = telebot.types.KeyboardButton('Нарочь')
-    button2 = telebot.types.KeyboardButton('Ялта')
-    button3 = telebot.types.KeyboardButton('Санкт-Петербург')
-    button4 = telebot.types.KeyboardButton('Москва')
-    markup.add(button)
-    markup.add(button1)
-    markup.add(button2)
-    markup.add(button3)
-    markup.add(button4)
+@dp.message_handler(commands="start")
+async def cmd_start(message: types.Message):
+    kb = [
+        [
+            types.KeyboardButton(text="Минск"),
+            types.KeyboardButton(text="Москва"),
+            types.KeyboardButton(text="Лондон"),
+            types.KeyboardButton(text="Париж"),
+            types.KeyboardButton(text="Ялта")
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
+    await message.answer("Выбери город и я покажу тебе сводку погоды!", reply_markup=keyboard)
+@dp.message_handler()
+async def get_weather(message: types.Message):
+    code_to_smile = {
+        "Clear": "Ясно \U00002600",
+        "Clouds": "Облачно \U00002601",
+        "Rain": "Дождь \U00002614",
+        "Drizzle": "Дождь \U00002614",
+        "Thunderstorm": "Гроза \U000026A1",
+        "Snow": "Снег \U0001F328",
+        "Mist": "Туман \U0001F32B"
+    }
 
-    bot.send_message(message.chat.id, 'Привет! Погода в городе:', reply_markup=markup)
+    try:
+        r = requests.get(
+            f"http://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={open_weather_token}&units=metric"
+        )
+        data = r.json()
+        city = data["name"]
+        cur_weather = data["main"]["temp"]
 
-@bot.message_handler(content_types='text')
-def text(message):
-    if message.text == 'USD' or message.text == 'EUR' or message.text == 'CNY':
-        res = getCourse(message.text)
-        bot.send_message(message.chat.id, res)
-    else:
-        bot.send_message(message.chat.id, 'Я так не умею')
+        weather_description = data["weather"][0]["main"]
+        if weather_description in code_to_smile:
+            wd = code_to_smile[weather_description]
+        else:
+            wd = "Посмотри в окно, не пойму что там за погода!"
+
+        humidity = data["main"]["humidity"]
+        pressure = data["main"]["pressure"]
+        wind = data["wind"]["speed"]
+        sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+        sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+        length_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
+            data["sys"]["sunrise"])
+
+        await message.reply(f"***{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
+              f"Погода в городе: {city}\nТемпература: {cur_weather}C° {wd}\n"
+              f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind} м/с\n"
+              f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\nПродолжительность дня: {length_of_the_day}\n"
+              f"***Хорошего дня!***"
+              )
+
+    except:
+        await message.reply("\U00002620 Проверьте название города \U00002620")
 
 
-def getCourse(cur):
-    response = requests.get('https://www.nbrb.by/api/exrates/rates/'+cur+'?parammode=2')
-    data = json.loads(response.text)
-    return data['Cur_OfficialRate']
-
-bot.polling()
+if __name__ == '__main__':
+    executor.start_polling(dp)
